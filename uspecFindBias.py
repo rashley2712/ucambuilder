c3a06063ef04381a98f1fc632acec8c49dba4eb7
Richard Ashley <rashley@gmail.com>
2015-04-29 11:05:57 +0100

    Counts frames

--- uspecFindBias.py
+++ uspecFindBias.py
@@ -4,33 +4,20 @@ try:
 	sys.path.remove('/home/astro/phsgan/Python64/lib/python/site-packages/astropy-0.3.2-py2.6-linux-x86_64.egg')
 except (ValueError):
 	print "No need to fix sys.path"
-import os
 import ultracamutils
-import matplotlib.pyplot
 import argparse
-import numpy, math
 import classes
 import ultraspecClasses
+import datetime
 from trm import ultracam
-from trm.ultracam.UErrors import PowerOnOffError, UendError, UltracamError
-import ultracam_shift
-import time, datetime
-import json
-from scipy import ndimage
-import Image
-import ucamObjectClass
-from photutils import datasets
-from photutils import daofind
-from photutils import aperture_photometry, CircularAperture, psf_photometry, GaussianPSF
-import astropy.table, astropy.io
-from astropy.stats import median_absolute_deviation as mad
-import astropy.stats.sigma_clipping
-from astropy.stats import sigma_clipped_stats
-from astropy.convolution import Gaussian2DKernel
-from photutils.detection import detect_sources
-from scipy.ndimage import binary_dilation
-from photutils.background import Background
-import re
+
+def getNumberOfFrames(runName):
+	runFilename = ultracamutils.addPaths(config.ULTRASPECRAW, runName)
+	rdat  = ultracam.Rdata(biasRunFilename, 1, server=False)
+	numberFrames = rdat.ntotal()
+	assert isinstance(numberFrames, int)
+	return numberFrames
+
 
 def checkWindowBinMatch(windows1, windows2):
 	if len(windows1) != len(windows2): return False
@@ -135,33 +122,10 @@ if __name__ == "__main__":
 		sys.exit()
 	
 	print "The bias for %s is %s."%(arg.runname, biasRun)
-	
-	
-	
+
+
+
 	sys.exit()
 		
 	
-	"""debug.write("Examining the first few frames of the run...", level = 2)
-	originalStackedFrame = numpy.zeros((1057, 1040))
-	
-	ccdFrame = rdat()
-	window = ccdFrame[0]
-	allWindows = []	
-	for windowIndex, w in enumerate(window):
-		# Set up some info about the window sizes and extents
-		window = ultraspecClasses.window()
-		window.setExtents(w.llx, w.lly, w.nx, w.ny)
-		window.setBinning(w.xbin, w.ybin)
-		
-		image = w._data
-		allWindows.append(window)
-		
-	(xmin, ymin, xmax, ymax) = ultracamutils.determineFullFrameSize(allWindows)
-	fullFramexsize = xmax - xmin
-	fullFrameysize = ymax - ymin
 
-	print "(xmin, xmax, ymin, ymax) (%d, %d, %d, %d)"%(xmin, xmax, ymin, ymax) 
-	
-	for w in allWindows:
-		print "Window:", w
-	"""
